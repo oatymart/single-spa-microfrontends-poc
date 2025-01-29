1af58f70223108cb5568349505092f866ba4fb82
Martin Nicholson <martin@taotesting.com>
2025-01-29 16:12:32 +0100

    Add working root-config

---
+++ host-app/root-config/src/oat-sa-root-config.js
@@ -0,0 +1,26 @@
+import { registerApplication, start } from "single-spa";
+import {
+  constructApplications,
+  constructRoutes,
+  constructLayoutEngine,
+} from "single-spa-layout";
+import microfrontendLayout from "./microfrontend-layout.html";
+
+const routes = constructRoutes(microfrontendLayout);
+const applications = constructApplications({
+  routes,
+  loadApp({ name }) {
+    return System.import(/* webpackIgnore: true */ name);
+  },
+});
+// registerApplication({
+//     name: "@oat-sa/mfe1",
+//     app: () => System.import("@oat-sa/mfe1"),
+//     activeWhen: "/",
+// });
+
+const layoutEngine = constructLayoutEngine({ routes, applications });
+
+applications.forEach(registerApplication);
+layoutEngine.activate();
+start();
